66ac82d78d1421500ff37f67433d19f99f9ec746
kjeon0901 <51190120+kjeon0901@users.noreply.github.com>
2019-11-05 15:30:48 +0900

    Add files via upload
    add makeaccount

--- home/Main.java
+++ home/Main.java
@@ -7,10 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("first.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("First.fxml"));
 		primaryStage.setScene(new Scene(root));
 		primaryStage.setTitle("Item Sharing Service");
 		primaryStage.show();
--- home/RootController.java
+++ home/RootController.java
@@ -1,6 +1,8 @@
 package home;
 
 import java.io.IOException;
+import home.Main;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,11 +13,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 
 public class RootController implements Initializable {
 
 	@FXML Button user;
 	@FXML Button manager;
+	@FXML TextField getID;
+	@FXML PasswordField getPW;
+	@FXML TextField getPhoneNum;
+	@FXML TextField getName;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -27,7 +35,7 @@ public class RootController implements Initializable {
 		Stage stage = (Stage)user.getScene().getWindow();
 		
 		try {
-			Parent second = FXMLLoader.load(getClass().getResource("login.fxml"));
+			Parent second = FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene sc = new Scene(second);
 			 newStage.setScene(sc);
 	         newStage.show();
@@ -40,5 +48,34 @@ public class RootController implements Initializable {
 	@FXML public void moveManager() {
 		
 	}
+
+	@FXML public void submitUserSignup() {
+		
+	}
+
+	@FXML public void goBackHome(Stage primaryStage) {
+		primaryStage.show();
+	}
+
+	@FXML public void checkExistID() {
+		
+	}
+
+	@FXML public void goUserSignUp() {
+		Stage newStage = new Stage();
+		Stage stage = (Stage)user.getScene().getWindow();
+		
+		try {
+			Parent third = FXMLLoader.load(getClass().getResource("MakeAccount.fxml"));
+			Scene sc = new Scene(third);
+			 newStage.setScene(sc);
+	         newStage.show();
+			stage.hide();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML public void goBackHome() {}
 
 }
