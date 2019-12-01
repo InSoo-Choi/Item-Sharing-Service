2c909116911310cb0df8c94761cbb7cb903b1619
h5jam <blessmealways00@gmail.com>
2019-12-02 02:33:39 +0900

    add data to tableview in userMain (임시)

--- database/DBItems.java
+++ database/DBItems.java
@@ -2,9 +2,14 @@ package database;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class DBItems {
 	
@@ -57,7 +62,49 @@ public class DBItems {
 		
 	}
 	
-	public static void loadItems() {
+	public static ObservableList loadItems() {
 		
+		Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        
+        ObservableList<ObservableList> ItemList = FXCollections.observableArrayList();
+        
+        try {
+        	conn = DBconnect.connect();
+        	stmt = conn.createStatement();
+        	 
+        	String sql = "SELECT post_num, post_name, kinds, post_by_id, limit_date From items";
+        	rs = stmt.executeQuery(sql);
+        	
+        	while(rs.next()) {
+        		ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added "+row );
+                ItemList.add(row);
+        	}
+        	
+        } catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally{
+            try{
+                if( conn != null && !conn.isClosed()){
+                    conn.close();
+                }
+                if( stmt != null && stmt.isClosed()){
+                    stmt.close();
+                }
+            }
+            catch( SQLException e){
+                e.printStackTrace();
+            }
+        }
+        
+        return ItemList;
 	}
 }
--- user/UserMainController.java
+++ user/UserMainController.java
@@ -3,6 +3,8 @@ package user;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -11,18 +13,28 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 
 
 public class UserMainController implements Initializable {
 	
 	@FXML Button moveMypage_Btn;
 	@FXML ComboBox<String> kinds;
+	@FXML TableView itemListTable;
+	@FXML TableColumn numCol;
+	@FXML TableColumn nameCol;
+	@FXML TableColumn idCol;
+	@FXML TableColumn limitCol;
 	
+	ObservableList itemList = database.DBItems.loadItems();
 	
 	@FXML public void moveMyPage() throws Exception {
 		Stage primaryStage = new Stage();
@@ -56,8 +68,36 @@ public class UserMainController implements Initializable {
 			stage.close();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void initialize(URL location, ResourceBundle resources){
+		
+		itemListTable.setItems(itemList);
+		
+		
+		numCol.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+            public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                return new SimpleStringProperty(param.getValue().get(0).toString());                        
+            }                    
+        });
+		
+		nameCol.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+            public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                return new SimpleStringProperty(param.getValue().get(1).toString());                        
+            }                    
+        });
+		
+		idCol.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+            public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                return new SimpleStringProperty(param.getValue().get(3).toString());                        
+            }                    
+        });
+		
+		limitCol.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+            public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                return new SimpleStringProperty(param.getValue().get(4).toString());                        
+            }                    
+        });
 
 	}
 
